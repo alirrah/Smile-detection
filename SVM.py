05b8b4b8d391602d9c232c6355a114c7067df546
alirrah <alir.rah81@gmail.com>
2023-05-20 03:27:01 +0330

    read labels

--- SVM.py
+++ SVM.py
@@ -5,10 +5,17 @@ class image:
     pass
 
 class svm:
-    def divide():
-        test = set()
+    def divide(self):
+        self.test = set()
 
-        while len(test) < (4000 / 100 * 30):
-	        test.add(random.randint(1, 4000))
+        while len(self.test) < (4000 / 100 * 30):
+            self.test.add(random.randint(1, 4000))
         
-        ## train = z(1, 4000) - test
+        # train = z(1, 4000) - test
+    
+    def readLabels(self):
+        self.labels = []
+
+        with open("labels.txt") as FILE:
+            for line in FILE:  
+                self.labels.append(int(line[0]))
