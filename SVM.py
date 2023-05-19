ad5d3248e1492e81fafa0fd4209dc5a055393691
alirrah <alir.rah81@gmail.com>
2023-05-20 03:13:30 +0330

    divide function

--- SVM.py
+++ SVM.py
@@ -1,6 +1,14 @@
+import random
+
 
 class image:
     pass
 
 class svm:
-    pass
+    def divide():
+        test = set()
+
+        while len(test) < (4000 / 100 * 30):
+	        test.add(random.randint(1, 4000))
+        
+        ## train = z(1, 4000) - test
