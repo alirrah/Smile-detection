80b4eb3ebc63bb19bc393068d0a96c3179166b11
alirrah <alir.rah81@gmail.com>
2023-05-20 03:33:17 +0330

    crop face

--- SVM.py
+++ SVM.py
@@ -1,8 +1,19 @@
 import random
-
+import cv2
 
 class image:
-    pass
+    def __init__(self, path):
+        self.img = cv2.imread(path)
+        
+    def cropImg(self):
+        grayImg = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        faceClassifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        face = faceClassifier.detectMultiScale(grayImg, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+        imgRgb = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        for (x, y, w, h) in face:
+            imgRgb = imgRgb[y:y+h, x:x+w]
+            break
+        self.img = imgRgb
 
 class svm:
     def divide(self):
